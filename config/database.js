020ad79658635580f565ec1ce5d234d5a740568a
sriram23 <sriramsubramanian23@gmail.com>
2020-08-02 13:15:00 +0530

    Added some piece of code

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+mongoose.connect(process.env.MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, (err) => {
     if(err)
         console.log('Unable to connect Mongo!!',err)
 });
